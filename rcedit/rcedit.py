557da7c41dd4104fcb5bae5ca7ddc86e67ff2a94
Thomas Grill <gr@grrrr.org>
2024-10-19 00:52:03 +0200

    Add page options get/set

--- rcedit/rcedit.py
+++ rcedit/rcedit.py
@@ -129,7 +129,6 @@ class RCEdit:
         if rtext.strip():
             raise RCException("page_options_set failed")
 
-
     license_options = {
         "all-rights-reserved", "cc-by", "cc-by-sa", "cc-by-nc", "cc-by-nc-sa", "cc-by-nc-nd", "public-domain"        
     }
